71202bae953abb4138b631639f3a96e0459fca01
mattdee <nap.rungon@gmail.com>
2026-09-24 13:14:05 +0800

    feat: prevent task form reload

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ const taskForm = document.querySelector("#task-form");
 taskForm.addEventListener("submit", handleTaskSubmit);
 
 function handleTaskSubmit(event) {
+    event.preventDefault();
+
     console.log("The task form was submitted.");
 }
